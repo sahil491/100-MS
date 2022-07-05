327a2d7c6a09c14301b1d20aae43feca6df1e5e8
sahil491 <sahusahil02@gmail.com>
2022-07-06 00:53:06 +0530

    100-MS project

---
+++ peerjs/src/App.js
@@ -0,0 +1,79 @@
+// import logo from './logo.svg';
+
+import Peer from 'peerjs';
+
+import './App.css';
+import { useEffect, useRef, useState } from 'react';
+
+
+function App() {
+  const [peerId, setPeerId] = useState("");
+  const [remotePeerIdValue, setRemotePeerIdValue] = useState("");
+const remoteVideoRef = useRef(null);
+const currentUserVideoRef = useRef(null);
+const peerInstance = useRef(null);
+
+
+ 
+useEffect(()=>{
+  const peer = new Peer();
+
+  peer.on('open', (id)=> {
+    // console.log('My peer ID is: ' + id);
+    setPeerId(id);
+  });
+  peer.on('call', (call) => {
+    var getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+getUserMedia({ video : true, audio : true}, function(mediaStream){
+  currentUserVideoRef.current.srcObject = mediaStream;
+  currentUserVideoRef.current.play();
+  call.answer(mediaStream)
+  call.on('stream', function(remoteStream){
+    remoteVideoRef.current.srcObject = remoteStream;
+    remoteVideoRef.current.play();
+  })
+
+  
+});
+  
+  
+  });
+  peerInstance.current = peer;
+}, [])
+const call = (remotePeerId)=>{
+
+var getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+getUserMedia({ video : true, audio : true}, (mediaStream)=>{
+
+  currentUserVideoRef.current.srcObject = mediaStream;
+  currentUserVideoRef.current.play();
+  const call = peerInstance.current.call(remotePeerId, mediaStream)
+
+  call.on('stream',  (remoteStream)=>{
+   
+    remoteVideoRef.current.srcObject = remoteStream;
+    remoteVideoRef.current.play();
+  });
+});
+}
+
+console.log(peerId);
+
+  return (
+    <div className="App">
+      <h1>Current User Id is {peerId}</h1>
+      <input type="text" value={remotePeerIdValue} onChange={e => setRemotePeerIdValue(e.target.value)} />
+      <button onClick={()=>call(remotePeerIdValue)}>Call</button>
+      <div>
+        <video ref={ currentUserVideoRef} />
+      </div>
+      <div>
+        <video ref={ remoteVideoRef}/>
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
